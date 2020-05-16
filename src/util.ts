19f9f4f45e36c3b566bc67f12789c30cd8c19c0f
Ceddy Muhoza <ceddymuhoza@gmail.com>
2020-05-16 01:03:07 -0400

    export utils

--- src/util.ts
+++ src/util.ts
@@ -1,20 +1,20 @@
 var C = require('./constants');
 
-function _findKeyForValue(object, value) {
+export function _findKeyForValue(object, value) {
   for (var key in object) {
     if (object[key] === value) {
       return key;
     }
   }
-  
+
   return C.TICK_TYPE.UNKNOWN;
 }
 
-function incomingToString(incoming) {
+export function incomingToString(incoming) {
   return _findKeyForValue(C.INCOMING, incoming);
 }
 
-function numberToString(number) {
+export function numberToString(number) {
   if (number === Number.MAX_VALUE) {
     return 'MAX';
   } else if (number === Number.MIN_VALUE) {
@@ -24,15 +24,10 @@ function numberToString(number) {
   }
 }
 
-function outgoingToString(outgoing) {
+export function outgoingToString(outgoing) {
   return _findKeyForValue(C.OUTGOING, outgoing);
 }
 
-function tickTypeToString(tickType) {
+export function tickTypeToString(tickType) {
   return _findKeyForValue(C.TICK_TYPE, tickType);
 }
-
-exports.incomingToString = incomingToString;
-exports.numberToString = numberToString;
-exports.outgoingToString = outgoingToString;
-exports.tickTypeToString = tickTypeToString;
