e6d403068a2c0143618864bdf779dacd5dd55c1b
Martin Kask <martin@industrial.ninja>
2021-02-21 23:02:27 +0200

    Fix lint rule

--- src/tests/jest.d.ts
+++ src/tests/jest.d.ts
@@ -1,4 +1,4 @@
-/* eslint-disable typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 declare namespace jest {
   interface Matchers<R> {
     toMatchSchema(schema: any): R;
