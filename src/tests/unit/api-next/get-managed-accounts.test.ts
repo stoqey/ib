3f55e6c28f25d2a1c5d30ed38e370a50731996d7
Matthias Frener <cmatt@gmx.net>
2021-04-01 09:45:38 +0200

    Add IBApiNext.getManagedAccounts (#65)
    Add IBApiNext.getManagedAccounts

---
+++ src/tests/unit/api-next/get-managed-accounts.test.ts
@@ -0,0 +1,37 @@
+/**
+ * This file implements tests for the [[IBApiNext.getManagedAccounts]] function.
+ */
+
+import {
+  IBApiNext,
+  IBApiAutoConnection,
+  IBApiNextError,
+  EventName,
+} from "../../..";
+
+describe("RxJS Wrapper: getManagedAccounts()", () => {
+  test("Promise result", (done) => {
+    // create IBApiNext
+
+    const apiNext = new IBApiNext();
+    const api = ((apiNext as unknown) as Record<string, unknown>)
+      .api as IBApiAutoConnection;
+
+    // emit a EventName.managedAccounts and verify RxJS result
+
+    apiNext
+      .getManagedAccounts()
+      .then((accounts) => {
+        expect(accounts.length).toEqual(3);
+        expect(accounts.find((v) => v === "U123456")).toEqual("U123456");
+        expect(accounts.find((v) => v === "U987654")).toEqual("U987654");
+        expect(accounts.find((v) => v === "U000000")).toEqual("U000000");
+        done();
+      })
+      .catch((error: IBApiNextError) => {
+        fail(error.error.message);
+      });
+
+    api.emit(EventName.managedAccounts, "U123456,U987654,U000000");
+  });
+});
---
+++ src/tools/managed-accts.ts
@@ -0,0 +1,58 @@
+/**
+ * This App will print the accounts to which the logged user has access to.
+ */
+
+import path from "path";
+
+import { IBApiNextError } from "../api-next";
+import logger from "../common/logger";
+import { IBApiNextApp } from "./common/ib-api-next-app";
+
+/////////////////////////////////////////////////////////////////////////////////
+// The help text.                                                              //
+/////////////////////////////////////////////////////////////////////////////////
+
+const DESCRIPTION_TEXT =
+  "Prints the accounts to which the logged user has access to.";
+const USAGE_TEXT = "Usage: managed-accts.js <options>";
+const OPTION_ARGUMENTS: [string, string][] = [];
+const EXAMPLE_TEXT = "managed-accts.js -port=4002";
+
+//////////////////////////////////////////////////////////////////////////////
+// The App code                                                             //
+//////////////////////////////////////////////////////////////////////////////
+
+class PrintManagedAcctsApp extends IBApiNextApp {
+  constructor() {
+    super(DESCRIPTION_TEXT, USAGE_TEXT, OPTION_ARGUMENTS, EXAMPLE_TEXT);
+  }
+
+  /**
+   * Start the the app.
+   */
+  start(): void {
+    const scriptName = path.basename(__filename);
+    logger.debug(`Starting ${scriptName} script`);
+    this.connect(0);
+    this.api
+      .getManagedAccounts()
+      .then((accounts) => {
+        this.printObject(accounts);
+        this.stop();
+      })
+      .catch((err: IBApiNextError) => {
+        this.error(`getManagedAccounts failed with '${err.error.message}'`);
+      });
+  }
+
+  /**
+   * Stop the the app with success code.
+   */
+  stop() {
+    this.exit();
+  }
+}
+
+// run the app
+
+new PrintManagedAcctsApp().start();
