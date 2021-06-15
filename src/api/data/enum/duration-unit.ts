015f6b92c39c26b25954a3bf5889077437ad197d
Matthias Frener <mfrener@cinemo.com>
2021-06-15 23:29:54 +0200

    Add IBApiNext.getHistogramData

---
+++ src/api/data/enum/duration-unit.ts
@@ -0,0 +1,12 @@
+/**
+ * Duration unit.
+ */
+export enum DurationUnit {
+  SECOND = "SECOND",
+  DAY = "DAY",
+  WEEK = "WEEK",
+  MONTH = "MONTH",
+  YEAR = "YEAR",
+}
+
+export default DurationUnit;
