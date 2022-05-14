1624d6940f7c6c369e4f49bc92202d0d8b3af212
Ronan-Yann Lorin <ryl@free.fr>
2022-05-14 20:32:14 +0200

    open-roder.ts recovered

---
+++ src/tools/open-orders.ts
@@ -0,0 +1,59 @@
+/**
+ * This App will print IBKR account open orders to console.
+ */
+
+import path from "path";
+import { Subscription } from "rxjs";
+
+import { IBApiNextError } from "../";
+import logger from "../common/logger";
+import { IBApiNextApp } from "./common/ib-api-next-app";
+
+/////////////////////////////////////////////////////////////////////////////////
+// The help text.                                                              //
+/////////////////////////////////////////////////////////////////////////////////
+
+const DESCRIPTION_TEXT = "Prints the account open orders.";
+const USAGE_TEXT = "Usage: open-orders.js <options>";
+const OPTION_ARGUMENTS: [string, string][] = [];
+const EXAMPLE_TEXT = "open-orders.js";
+
+//////////////////////////////////////////////////////////////////////////////
+// The App code                                                             //
+//////////////////////////////////////////////////////////////////////////////
+
+class OpenOrdersApp extends IBApiNextApp {
+  constructor() {
+    super(DESCRIPTION_TEXT, USAGE_TEXT, OPTION_ARGUMENTS, EXAMPLE_TEXT);
+  }
+
+  /**
+   * Start the app.
+   */
+  start(): void {
+    const scriptName = path.basename(__filename);
+    logger.debug(`Starting ${scriptName} script`);
+
+    this.connect(this.cmdLineArgs.watch ? 10000 : 0);
+    this.api
+      .getAllOpenOrders()
+      .then((orders) => {
+        this.printObject(orders);
+        this.stop();
+      })
+      .catch((err: IBApiNextError) => {
+        this.error(`getAllOpenOrders failed with '${err}'`);
+      });
+  }
+
+  /**
+   * Stop the app with success code.
+   */
+  stop() {
+    this.exit();
+  }
+}
+
+// run the app
+
+new OpenOrdersApp().start();
