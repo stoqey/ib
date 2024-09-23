8e200b9e439308b3a599c611b61811c554eea6ca
CoCoding <127168299+icocoding@users.noreply.github.com>
2024-09-23 12:36:26 +0800

    Update logger.ts

--- src/core/api-next/logger.ts
+++ src/core/api-next/logger.ts
@@ -20,6 +20,7 @@ export class IBApiNextLogger {
   /** Set the current log level. */
   set logLevel(level: LogLevel) {
     this._logLevel = level;
+    this.logger.logLevel = level;
   }
 
   /** Log a debug information. */
