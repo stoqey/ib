6085bf49cf9baa34b445c1276954de63aff6d96b
ceddybi <ceddymuhoza@gmail.com>
2025-03-09 00:51:14 -0500

    fix(TickByTickAllLast): eslint

--- src/api/market/tickByTickAllLast.ts
+++ src/api/market/tickByTickAllLast.ts
@@ -6,21 +6,21 @@ import { TickAttribLast } from "../historical/historicalTickLast";
  * Used when requesting tick-by-tick data with tickType = LAST or ALL_LAST.
  */
 export interface TickByTickAllLast {
-    /** The UNIX timestamp of the tick. */
-    time: number;
+  /** The UNIX timestamp of the tick. */
+  time: number;
 
-    /** The price of the tick. */
-    price: number;
+  /** The price of the tick. */
+  price: number;
 
-    /** The size of the tick. */
-    size: number;
+  /** The size of the tick. */
+  size: number;
 
-    /** The tick attributes of the tick. */
-    tickAttribLast: TickAttribLast;
+  /** The tick attributes of the tick. */
+  tickAttribLast: TickAttribLast;
 
-    /** The exchange of the tick. */
-    exchange: string;
+  /** The exchange of the tick. */
+  exchange: string;
 
-    /** The special conditions of the tick. */
-    specialConditions: string;
+  /** The special conditions of the tick. */
+  specialConditions: string;
 }
